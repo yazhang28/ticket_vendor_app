f37e0664ebd050e58d6b1b2151974c176c68f170
Yao Zhang <yaozhang@yaos-mbp.lan>
2019-03-02 22:33:00 -0600

    add sql for additional tables

--- db/create_table.sql
+++ db/create_table.sql
@@ -1,32 +1,37 @@
-/* ------- buyer_type ------- */
+/* ------- buyer_referral_type ------- */
   CREATE TABLE IF NOT EXISTS buyer_referral_type (
     id SERIAL,
-    type VARCHAR(50) NOT NULL,
+    type VARCHAR(50) UNIQUE NOT NULL,
     PRIMARY KEY (id)
   );
 
+  INSERT INTO buyer_referral_type (type)
+    VALUES ('Internal'),
+           ('Other');
+
 /* ------- buyer ------- */
   CREATE TABLE IF NOT EXISTS buyer (
-    email_address VARCHAR(50) CONSTRAINT email_address CHECK (email_address ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9-]+[.][A-Za-z]+$'),
+    id SERIAL,
     buyer_referral_type_id BIGINT NOT NULL REFERENCES buyer_referral_type,
+    email_address VARCHAR(50) UNIQUE CONSTRAINT email_address CHECK (email_address ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9-]+[.][A-Za-z]+$'),
     first_name VARCHAR(50) NOT NULL CONSTRAINT first_name CHECK (first_name ~* '[a-zA-Z]+'),
     last_name VARCHAR(50) NOT NULL CONSTRAINT last_name CHECK ( last_name ~* '[a-zA-Z]+'),
     phone_number VARCHAR(50) NOT NULL,
     active BOOLEAN NOT NULL DEFAULT TRUE,
-    PRIMARY KEY (email_address)
+    PRIMARY KEY (id)
   );
 
 /* ------- payment_method ------- */
   CREATE TABLE IF NOT EXISTS payment_method (
     id SERIAL,
-    buyer_email_address VARCHAR(50) NOT NULL REFERENCES buyer,
+    buyer_id INT NOT NULL REFERENCES buyer,
     billing_address VARCHAR(100) CONSTRAINT billing_address CHECK (billing_address ~ '[\sA-Za-z0-9.]+'),
     city VARCHAR(100),
     postal_code VARCHAR(10) CONSTRAINT postal_code CHECK (postal_code ~ '[0-9]'),
     credit_card_number VARCHAR(20) UNIQUE,
     security_code VARCHAR(4) CONSTRAINT security_code CHECK (security_code ~ '[0-9]'),
     month_exp INT CONSTRAINT month_exp CHECK (month_exp <=12 AND month_exp >= 1),
-    year_exp INT,
+    year_exp INT CONSTRAINT year_exp CHECK (year_exp >= extract(YEAR FROM CURRENT_DATE)::INT),
     internal_payment_method BOOLEAN NOT NULL DEFAULT FALSE,
     external_payment_method BOOLEAN NOT NULL DEFAULT FALSE,
     active BOOLEAN NOT NULL DEFAULT TRUE,
@@ -36,7 +41,7 @@
 /* ------- city_type ------- */
   CREATE TABLE IF NOT EXISTS city_type (
     id SERIAL,
-    city VARCHAR(100) NOT NULL,
+    city VARCHAR(100) UNIQUE NOT NULL,
     PRIMARY KEY (id)
   );
 
@@ -45,6 +50,34 @@
     id SERIAL,
     date DATE NOT NULL,
     time TIME WITH TIME ZONE NOT NULL,
+    city_type_id INT NOT NULL REFERENCES city_type,
     PRIMARY KEY (id)
   );
 
+/* ------- ticket ------- */
+CREATE TABLE IF NOT EXISTS ticket (
+  id SERIAL,
+  event_id BIGINT NOT NULL REFERENCES event,
+  buyer_id INT REFERENCES buyer,
+  row VARCHAR(10) NOT NULL,
+  section INT NOT NULL,
+  price INT NOT NULL,
+  sold BOOLEAN NOT NULL DEFAULT FALSE,
+  date_sold TIMESTAMP,
+  delivery_by_email BOOLEAN,
+  delivery_by_phone BOOLEAN,
+  CONSTRAINT required_fields_for_sale CHECK (
+    (date_sold IS NULL
+       AND buyer_id IS NULL
+       AND delivery_by_email IS NULL
+       AND delivery_by_phone IS NULL)
+    OR (date_sold IS NOT NULL
+      AND buyer_id IS NOT NULL
+      AND delivery_by_email IS NOT NULL
+      AND delivery_by_phone IS NULL)
+    OR (date_sold IS NULL
+      AND buyer_id IS NOT NULL
+      AND delivery_by_email IS NULL
+      AND delivery_by_phone IS NOT NULL)
+    )
+);
---
+++ db/db_test.sql
@@ -0,0 +1,37 @@
+/* ===================== positve passed test ===================== */
+INSERT INTO buyer (email_address, buyer_referral_type_id, first_name, last_name, phone_number)
+VALUES ('test@testing.com',1,'joe','smith','234'),
+       ('apple@orange.com',1,'apple','orange','1');
+INSERT INTO payment_method (buyer_id, billing_address, city, postal_code, credit_card_number, security_code, month_exp, year_exp)
+VALUES ('1', '123 apple.ave p.0 box 34', 'pumpkinville', '0123456789', '1234123412345', '1234', 1, 2019);
+
+INSERT INTO city_type (city) VALUES ('Montreal');
+INSERT INTO event (date, time, city_type_id) VALUES ('01/02/2019', '01:01:01', 1);
+
+INSERT INTO ticket (event_id, row, section, price)
+VALUES (4, 'A1', 23, 45);
+
+UPDATE ticket SET
+  buyer_id = 1,
+  date_sold = current_timestamp,
+  delivery_by_email = TRUE
+WHERE event_id = 4;
+
+UPDATE ticket SET
+  sold = TRUE
+WHERE buyer_id = 1;
+
+-- INSERT INTO ticket (event_id, buyer_email_adress, row, section, price, date_sold, delivery_by_mail, delivery_by_phone)
+-- VALUES
+
+/* ===================== positve failed test ===================== */
+
+-- email, postal_code, credit_card_number, security_code, month_exp, date_exp
+INSERT INTO payment_method (buyer_email_address, billing_address, city, postal_code, credit_card_number, security_code, month_exp, year_exp)
+VALUES ('A@B.COM', '123 apple.ave p.0 box 34', 'pumpkinville', '012345678910', '1234123412345', '12345', 13, 2014);
+
+-- missing required fields when purchasing: delivery_by_email, delivery_by_phone
+UPDATE ticket SET
+  buyer_id = 1,
+  date_sold = current_timestamp
+WHERE event_id = 4;
